e8670db713e807497b3d04ff6a1fa400183eed2f
nilesh-bhardwaj <nilesh-bhardwaj@csgroupchd.com>
2024-05-20 15:25:44 +0530

    Last Updates

--- resources/js/components/Checkout.jsx
+++ resources/js/components/Checkout.jsx
@@ -1,9 +1,77 @@
-import React from "react";
+import React, { useState } from "react";
 
-function Checkout()
-{
-    return(
-        <h1>Checkout Page</h1>
+const Checkout = () => {
+    const [collapseOne, setCollapseOne] = useState(true);
+    const [collapseTwo, setCollapseTwo] = useState(false);
+
+    return (
+        <div className="container d-flex justify-content-center mt-5 mb-5">
+            <div className="row g-3">
+                <div className="col-md-61">
+                    <span style={{fontSize:"21px" , marginLeft:"130px"}}><strong>Payment Method</strong></span>
+                    <div className="card">
+                        <div className="accordion" id="accordionExample">
+                            {collapseTwo && (
+                                <div className="card-body">
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        placeholder="Paypal email"
+                                    />
+                                </div>
+                            )}
+                        </div>
+                        {collapseOne && (
+                            <div className="card-body payment-card-body">
+                                <span className="font-weight-normal card-text">
+                                   <strong> Card Number</strong>
+                                </span>
+                                <div className="input">
+                                    <i className="fa fa-credit-card"></i>
+                                    <input
+                                        type="text"
+                                        className="form-control"
+                                        placeholder="0000 0000 0000 0000"
+                                    />
+                                </div>
+                                <div className="row mt-3 mb-3">
+                                    <div className="col-md-6">
+                                        <span className="font-weight-normal card-text">
+                                           <strong> Expiry Date</strong>
+                                        </span>
+                                        <div className="input">
+                                            <i className="fa fa-calendar"></i>
+                                            <input
+                                                type="text"
+                                                className="form-control"
+                                                placeholder="MM/YY"
+                                            />
+                                        </div>
+                                    </div>
+                                    <div className="col-md-6">
+                                        <span className="font-weight-normal card-text">
+                                          <strong>  CVC/CVV</strong>
+                                        </span>
+                                        <div className="input">
+                                            <i className="fa fa-lock"></i>
+                                            <input
+                                                type="text"
+                                                className="form-control"
+                                                placeholder="CVC/CVV"
+                                            />
+                                        </div>
+                                    </div>
+                                </div>
+                                <div>
+                                    <button>Pay Now</button>
+                                </div>
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </div>
+        </div>
     );
-}
+};
+
 export default Checkout;
